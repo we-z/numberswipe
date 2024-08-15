1d095c9603c411241eb36b9a8a7bf923f539087b
we-z <yousefsalem00@gmail.com>
2024-08-14 21:23:46 -0700

    Initial Commit

---
+++ numberswipe/numberswipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  numberswipeApp.swift
+//  numberswipe
+//
+//  Created by Wheezy Capowdis on 8/14/24.
+//
+
+import SwiftUI
+
+@main
+struct numberswipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
