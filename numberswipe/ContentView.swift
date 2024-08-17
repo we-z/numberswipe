e6d64ef2153fa7b6ce6f4e68362df704f876a40a
we-z <yousefsalem00@gmail.com>
2024-08-17 13:16:57 -0700

    first commit

--- numberswipe/ContentView.swift
+++ numberswipe/ContentView.swift
@@ -7,13 +7,32 @@
 
 import SwiftUI
 
+let deviceHeight = UIScreen.main.bounds.height
+let deviceWidth = UIScreen.main.bounds.width
+
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("2331")
+                .bold()
+                .lineLimit(1)
+                .minimumScaleFactor(0.1)
+                .font(.system(size: deviceWidth))
+                .frame(height: deviceHeight/4)
+            Spacer()
+            Text("52 + 43")
+                .bold()
+                .lineLimit(1)
+                .minimumScaleFactor(0.1)
+                .font(.system(size: deviceWidth))
+                .frame(height: deviceHeight/4)
+            Spacer()
+            Text("9333")
+                .bold()
+                .lineLimit(1)
+                .minimumScaleFactor(0.1)
+                .font(.system(size: deviceWidth))
+                .frame(height: deviceHeight/4)
         }
         .padding()
     }
