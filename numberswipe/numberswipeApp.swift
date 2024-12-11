d4e045af7815ed4c794e2ce1baed211a85406269
we-z <yousefsalem00@gmail.com>
2024-12-11 00:46:32 -0800

    remove home indicator

--- numberswipe/numberswipeApp.swift
+++ numberswipe/numberswipeApp.swift
@@ -12,6 +12,18 @@ struct numberswipeApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .prefersPersistentSystemOverlaysHidden()
+        }
+    }
+}
+
+extension View {
+
+    func prefersPersistentSystemOverlaysHidden() -> some View {
+        if #available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *) {
+            return self.persistentSystemOverlays(.hidden)
+        } else {
+            return self
         }
     }
 }
