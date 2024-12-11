7cc4353d4fb3737e5bf45d59ae9ee4df569dbcbb
we-z <yousefsalem00@gmail.com>
2024-12-10 23:01:14 -0800

    working game

--- numberswipe/ContentView.swift
+++ numberswipe/ContentView.swift
@@ -1,42 +1,123 @@
-//
-//  ContentView.swift
-//  numberswipe
-//
-//  Created by Wheezy Capowdis on 8/14/24.
-//
-
 import SwiftUI
 
-let deviceHeight = UIScreen.main.bounds.height
-let deviceWidth = UIScreen.main.bounds.width
-
 struct ContentView: View {
+    @State private var currentPower = 1
+    @State private var centerNumber = 2
+    @State private var topNumber = 4
+    @State private var bottomNumber = 3
+    @State private var isGameOver = false
+    @State private var chosenDirection: CGFloat = 0
+    @State private var scale: CGFloat = 1
+    @State private var bgColor = Color.black
+    
     var body: some View {
-        VStack {
-            Text("2331")
-                .bold()
-                .lineLimit(1)
-                .minimumScaleFactor(0.1)
-                .font(.system(size: deviceWidth))
-                .frame(height: deviceHeight/4)
-            Spacer()
-            Text("52 + 43")
-                .bold()
-                .lineLimit(1)
-                .minimumScaleFactor(0.1)
-                .font(.system(size: deviceWidth))
-                .frame(height: deviceHeight/4)
-            Spacer()
-            Text("9333")
-                .bold()
-                .lineLimit(1)
-                .minimumScaleFactor(0.1)
-                .font(.system(size: deviceWidth))
-                .frame(height: deviceHeight/4)
+        GeometryReader { g in
+            ZStack {
+                bgColor.ignoresSafeArea()
+                if isGameOver {
+                    VStack {
+                        Text("Game Over")
+                            .foregroundColor(.white)
+                            .font(.system(size: g.size.width * 0.15))
+                            .padding(.bottom, 20)
+                        Button("Reset") {
+                            reset()
+                        }
+                        .foregroundColor(.gray)
+                        .font(.system(size: g.size.width * 0.1))
+                    }
+                } else {
+                    VStack {
+                        // Top number
+                        Text("\(topNumber)")
+                            .minimumScaleFactor(0.01)
+                            .foregroundColor(.white)
+                            .font(.system(size: g.size.height * 0.15))
+                        Spacer()
+                        // Center number
+                        Text("\(centerNumber)")
+                            .minimumScaleFactor(0.01)
+                            .foregroundColor(.white)
+                            .font(.system(size: g.size.height * 0.6))
+                            .scaleEffect(scale)
+                            .offset(y: chosenDirection * (g.size.height / 2.0))
+                            .gesture(
+                                DragGesture(minimumDistance: 50)
+                                    .onEnded { val in
+                                        if abs(val.translation.height) > abs(val.translation.width) {
+                                            swipe(val.translation.height < 0, g)
+                                        }
+                                    }
+                            )
+                        Spacer()
+                        // Bottom number
+                        Text("\(bottomNumber)")
+                            .minimumScaleFactor(0.01)
+                            .foregroundColor(.white)
+                            .font(.system(size: g.size.height * 0.15))
+                    }
+                }
+            }
         }
-        .padding()
+    }
+    
+    func swipe(_ up: Bool, _ g: GeometryProxy) {
+        withAnimation(.easeIn(duration: 0.2)) {
+            chosenDirection = up ? -1 : 1
+            scale = 0.0
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            checkAnswer(correct: (up && topNumber == nextPower()) || (!up && bottomNumber == nextPower()))
+        }
+    }
+    
+    func checkAnswer(correct: Bool) {
+        if correct {
+            flash(color: .blue)
+            currentPower += 1
+            centerNumber = Int(pow(2, Double(currentPower)))
+            setNewNumbers()
+        } else {
+            flash(color: .red)
+            isGameOver = true
+        }
+        chosenDirection = 0
+        scale = 1
+    }
+    
+    func nextPower() -> Int {
+        return Int(pow(2, Double(currentPower+1)))
+    }
+    
+    func setNewNumbers() {
+        let correctVal = nextPower()
+        let offset = Int.random(in: 1...max(3,currentPower))
+        let incorrectVal = Bool.random() ? correctVal + offset : max(1, correctVal - offset)
+        if Bool.random() {
+            topNumber = correctVal
+            bottomNumber = incorrectVal
+        } else {
+            topNumber = incorrectVal
+            bottomNumber = correctVal
+        }
+    }
+    
+    func reset() {
+        bgColor = .black
+        currentPower = 1
+        centerNumber = 2
+        isGameOver = false
+        setNewNumbers()
+    }
+    
+    func flash(color: Color) {
+        bgColor = color
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            bgColor = .black
+        }
     }
 }
+
 
 #Preview {
     ContentView()
--- numberswipeUITests/numberswipeUITestsLaunchTests.swift
+++ numberswipeUITests/numberswipeUITestsLaunchTests.swift
@@ -8,7 +8,6 @@
 import XCTest
 
 final class numberswipeUITestsLaunchTests: XCTestCase {
-
     override class var runsForEachTargetApplicationUIConfiguration: Bool {
         true
     }
